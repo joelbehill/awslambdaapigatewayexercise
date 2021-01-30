2dc9550688e91b18bd9b3ea3f8950bc20e8b59b7
Joel Hill <joel.hill@gmail.com>
2021-01-30 09:24:41 -0600

    Testing to make sure ip additions and removals are possible

--- updatebadip.js
+++ updatebadip.js
@@ -8,13 +8,13 @@ const path = require('path')
 function difference(setA, setB) {
   let _difference = new Set(setA)
   for (let elem of setB) {
-      _difference.delete(elem)
+    _difference.delete(elem)
   }
   return _difference
 }
 
 /*
-We return
+We return a set based on the file array that is sent in
 */
 async function processFiles(fileNames, filePath) {
   let badIpAddressSet = new Set()
@@ -32,22 +32,25 @@ async function processFiles(fileNames, filePath) {
   return badIpAddressSet
 }
 
-async function processDynamo() {
-  const IpAddress = dynamoose.model('badips', { ipaddress: String })
-
+/*
+Go through all "rows" and assign each IP address to a set to return
+*/
+async function processDynamo(dynamooseModel) {
   let badIpAddressSet = new Set()
 
-  const scanResult = await IpAddress.scan().exec()
+  const scanResult = await dynamooseModel.scan().exec()
 
   for (let row of scanResult) {
     const ipAddress = row['ipaddress']
-    console.log(ipAddress)
+    badIpAddressSet.add(ipAddress)
   }
 
   return badIpAddressSet
 }
 
 exports.handler =  async function(event, context) {
+  const filePath = '/tmp/badips'
+
   const IpAddress = dynamoose.model('badips', { ipaddress: String })
 
   let message = 'Unable to update the database'
@@ -58,11 +61,25 @@ exports.handler =  async function(event, context) {
   // save the files to s3 but since the only thing we need the files for is to read them and
   // put their ip addresses into Dynamo
   try {
-    const filePath = '/tmp/badips'
     // await git().silent(true).clone('https://github.com/firehol/blocklist-ipsets.git', filePath)
+
+    // We need to get all of the files in the filePath directory
     const fileNames = fs.readdirSync(filePath).filter(el => path.extname(el) === '.ipset')
     const fileSet = await processFiles(fileNames, filePath)
-    const dynamodbSet = await processDynamo()
+    const dynamodbSet = await processDynamo(IpAddress)
+
+    const addIps = difference(fileSet, dynamodbSet)
+    const removeIps = difference(dynamodbSet, fileSet)
+
+    addIps.forEach(async ipAddress => {
+      await IpAddress.create({'ipaddress': ipAddress})
+    })
+
+    removeIps.forEach(async ipAddress => {
+      await IpAddress.delete(ipAddress)
+    })
+
+    message = 'We were able to update the database'
   } catch (error) {
     console.log(error)
   }
